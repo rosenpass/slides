a002bc5850e98b95c76c202cb29888b352f3c080
Karolin Varner <karo@cupdev.net>
2024-09-19 20:22:21 +0200

    feat: fix flake for eacn talk

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
             eso-pic biblatex biblatex-trad biblatex-software xkeyval xurl
             xifthen biber dirtytalk tikzsymbols pgfornament pgfopts bbding
             transparent pdfpc hyperxmp luacode luatexbase adjustbox
-            tikzfill caption qrcode upquote breakurl accsupp;
+            tikzfill caption qrcode upquote breakurl accsupp metafont;
         };
 
         documents = (builtins.fromTOML (builtins.readFile ./documents.toml)).documents;
